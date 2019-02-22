55dfb4073ac53570908420149f3607dbef754751
hassan <hassanali5120@gmail.com>
2019-02-22 11:40:41 +0500

    tested again the solution

---
+++ assets/js/addInventory.js
@@ -0,0 +1,38 @@
+(function ($) {
+
+
+    $(document).ready(function () {
+        //Add inventory
+        $('#id_contractID').click(function () {
+
+           contractID=this.value;
+           reqURL=window.location.origin+'/getContractDetail/';
+            formData=$('#addinventory_form').serialize();
+           $.ajax({
+                  type: "POST",
+                  url: reqURL,
+                  data: formData,
+           }).done(function (data) {
+               console.log(data);
+
+               weightPerBag=data.contractDetails.weightPerBag;
+               conesPerBag=data.contractDetails.conesPerBag;
+               noOfAdditional=data.contractDetails.noOfAdditional;
+               weightPerCone=data.contractDetails.weightPerCone;
+               conesPerBag=data.contractDetails.conesPerBag;
+               noOfBags=data.contractDetails.noOfBags;
+               totalWeight=(noOfAdditional*weightPerCone)+(weightPerBag*noOfBags);
+
+               console.log("cones"+noOfAdditional+"*"+conesPerBag);
+
+
+
+               console.log(totalWeight);
+
+
+           });
+        });
+
+    });
+})(django.jQuery);
+
