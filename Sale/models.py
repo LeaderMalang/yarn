22843f7f1048a43d942ea558bc04ec7615f4eecb
hassan <hassanali5120@gmail.com>
2019-03-14 16:20:20 +0500

    Completed Sales Model Refactoring.

--- Sale/models.py
+++ Sale/models.py
@@ -7,84 +7,83 @@ from  django.utils.timezone import now
 # Create your models here.
 
 class customers(models.Model):
-    companyName = models.CharField(max_length=30, verbose_name='Company Name')
-    companyAddress = models.CharField(max_length=50, verbose_name='Company Address')
-    city = models.CharField(max_length=20, verbose_name='City')
-    region = models.CharField(max_length=20, verbose_name='Region')
-    country = CountryField()
-    postalCode = models.IntegerField(verbose_name='Postal Code')
-    companyPhone = models.IntegerField(verbose_name="Company Phone")
-    companyFax = models.IntegerField(verbose_name='Company Fax')
-    elementaryID = models.ForeignKey(elementaryhead, on_delete=models.CASCADE, default=None, null=True,
+    Company_Name = models.CharField(max_length=30, verbose_name='Company Name')
+    Company_Address = models.CharField(max_length=50, verbose_name='Company Address')
+    City = models.CharField(max_length=20, verbose_name='City')
+    Region = models.CharField(max_length=20, verbose_name='Region')
+    Country = CountryField()
+    Postal_Code = models.IntegerField(verbose_name='Postal Code')
+    Company_Phone = models.IntegerField(verbose_name="Company Phone")
+    Company_Fax = models.IntegerField(verbose_name='Company Fax')
+    Elementary_Head = models.ForeignKey(elementaryhead, on_delete=models.CASCADE, default=None, null=True,
                                      verbose_name='Elementary head', editable=False)
 
-    website = models.URLField(default=None, verbose_name='Website')
+    Home_Page = models.URLField(default=None, verbose_name='Website')
     def save(self, force_insert=False, force_update=False, using=None,
              update_fields=None):
         accountID = elementaryhead.objects.order_by('id').last().id + 1
         code = '00001-000010-0000' + str(accountID)
         subhead=10
-        elementaryhead.objects.create(subhead_id=subhead, name=self.companyName,fixed=False, codes=code, right=True)
+        elementaryhead.objects.create(subhead_id=subhead, name=self.Company_Name,Fixed_Flag=False, Account_Codes=code, Right=True)
         accountID = elementaryhead.objects.order_by('id').last().id + 1
         subhead=11
         code = '00001-000011-0000' + str(accountID)
-        elementaryhead.objects.create(subhead_id=subhead, name=self.companyName+'(I/TAX PAYABLE)',fixed=False, codes=code, right=True)
+        elementaryhead.objects.create(subhead_id=subhead, name=self.Company_Name+'(I/TAX PAYABLE)',Fixed_Flag=False, Account_Codes=code, Right=True)
         accountID = elementaryhead.objects.order_by('id').last().id + 1
         subhead=15
         code = '00001-000015-0000' + str(accountID)
-        elementaryhead.objects.create(subhead_id=subhead, name=self.companyName+'(GST W/HELD PAYABLE)',fixed=False, codes=code, right=True)
-        self.supplier = True
-        self.buyer = False
+        elementaryhead.objects.create(subhead_id=subhead, name=self.Company_Name+'(GST W/HELD PAYABLE)',Fixed_Flag=False, Account_Codes=code, Right=True)
+
         super(customers,self).save()
     def __str__(self):
         return self.companyName
 
 class Contacts(models.Model):
-    customerID=models.ForeignKey(customers,on_delete=models.CASCADE,verbose_name='Customer Company')
-    firstName=models.CharField(max_length=35,verbose_name='First Name')
-    lastName=models.CharField(max_length=35,verbose_name='Last Name')
-    suffix=models.CharField(max_length=10,verbose_name='Suffix')
+    CustomerID=models.ForeignKey(customers,on_delete=models.CASCADE,verbose_name='Customer Company')
+    First_Name=models.CharField(max_length=35,verbose_name='First Name')
+    Last_Name=models.CharField(max_length=35,verbose_name='Last Name')
+    Suffix=models.CharField(max_length=10,verbose_name='Suffix')
     My_CHOICES=(('mr','MR'),('ms','MS'))
-    title=models.CharField(max_length=10,verbose_name='Title',choices=My_CHOICES)
-    phone1=models.BigIntegerField(verbose_name='Phone')
-    phone2=models.BigIntegerField(verbose_name='Another Phone',default=None)
-    mobile1=models.BigIntegerField(verbose_name='Mobile')
-    mobile2=models.BigIntegerField(verbose_name='Another Mobile',default=None)
-    email=models.EmailField()
-    fax=models.IntegerField(default=None,verbose_name='Fax')
+    Title=models.CharField(max_length=10,verbose_name='Title',choices=My_CHOICES)
+    Phone_1=models.BigIntegerField(verbose_name='Phone')
+    Phone_2=models.BigIntegerField(verbose_name='Another Phone',default=None)
+    Mobile_1=models.BigIntegerField(verbose_name='Mobile')
+    Mobile_2=models.BigIntegerField(verbose_name='Another Mobile',default=None)
+    Email=models.EmailField()
+    Fax=models.IntegerField(default=None,verbose_name='Fax')
 
     def __str__(self):
-        return self.customerID
+        return self.CustomerID
 
 
 
 class contracts(models.Model):
-    productDetailID=models.ForeignKey(productDetails,related_name='+',on_delete=models.CASCADE,verbose_name='Select Product',default=None)
-    customerID=models.ForeignKey(customers,on_delete=models.CASCADE,verbose_name='Select Customer',default=None)
-    totalUnits=models.IntegerField(verbose_name='Total Units',editable=False,default=None)
-    ratePerUnit=models.IntegerField(verbose_name='Rate Per Unit',default=None)
-    saleTax=models.IntegerField(verbose_name='Sale Tax',default=None)
-    incomeTax=models.IntegerField(verbose_name='Income Tax',default=None)
-    saleTaxwithHeld=models.IntegerField(verbose_name='Sale Tax with Held',default=None)
-    startDate=models.DateField(verbose_name='Start Date',default=None)
-    endDate=models.DateField(verbose_name='End Date',default=None)
-    manulContractNumber=models.IntegerField(verbose_name='Manul Contract Number',default=None)
-    paymentDays=models.IntegerField(verbose_name='Payment Days',default=None)
-    remarks=models.CharField(verbose_name='Remarks',max_length=100,default=None)
-    dateOfEntry=models.DateField(editable=False,default=now)
+    Product_Detail_ID=models.ForeignKey(productDetails,related_name='+',on_delete=models.CASCADE,verbose_name='Select Product',default=None)
+    Customer_ID=models.ForeignKey(customers,on_delete=models.CASCADE,verbose_name='Select Customer',default=None)
+    Total_Units=models.IntegerField(verbose_name='Total Units',editable=False,default=None)
+    Rate_Per_Unit=models.IntegerField(verbose_name='Rate Per Unit',default=None)
+    Sale_Tax=models.IntegerField(verbose_name='Sale Tax',default=None)
+    Income_Tax=models.IntegerField(verbose_name='Income Tax',default=None)
+    Sale_Tax_with_Held=models.IntegerField(verbose_name='Sale Tax with Held',default=None)
+    Start_Date=models.DateField(verbose_name='Start Date',default=None)
+    End_Date=models.DateField(verbose_name='End Date',default=None)
+    Manul_Contract_Number=models.IntegerField(verbose_name='Manul Contract Number',default=None)
+    Payment_Days=models.IntegerField(verbose_name='Payment Days',default=None)
+    Remarks=models.CharField(verbose_name='Remarks',max_length=100,default=None)
+    Date_Of_Entry=models.DateField(editable=False,default=now)
     def __str__(self):
-        return self.productDetailID
+        return self.Product_Detail_ID
 
 
 
 
 
 class contractDetails(models.Model):
-    contractID=models.ForeignKey(contracts,on_delete=models.CASCADE,verbose_name='Select Contract')
-    weightPerBag=models.IntegerField(verbose_name='Weight Per Bag')
-    conesPerBag=models.IntegerField(verbose_name='Cones Per Bag')
-    weightPerCone=models.IntegerField(verbose_name='Weight Per Cone')
-    noOfBags=models.IntegerField(verbose_name='No of Bags')
-    noOfAdditional=models.IntegerField(verbose_name='No of Additional Cones')
+    Contract_ID=models.ForeignKey(contracts,on_delete=models.CASCADE,verbose_name='Select Contract')
+    Weight_Per_Bag=models.IntegerField(verbose_name='Weight Per Bag')
+    Cones_Per_Bag=models.IntegerField(verbose_name='Cones Per Bag')
+    Weight_Per_Cone=models.IntegerField(verbose_name='Weight Per Cone')
+    No_Of_Bags=models.IntegerField(verbose_name='No of Bags')
+    No_Of_Additional_Cones=models.IntegerField(verbose_name='No of Additional Cones')
     def __str__(self):
-        return self.contractID
+        return self.Contract_ID
